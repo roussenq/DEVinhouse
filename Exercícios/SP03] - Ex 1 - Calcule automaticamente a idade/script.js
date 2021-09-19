b47d53604e60e9e6610cd4e88c5be643f966290d
David <davidroussenq@gmail.com>
2021-09-19 20:26:06 -0300

    exercicio 01
    realizado calculo da idade

---
+++ Exercícios/SP03] - Ex 1 - Calcule automaticamente a idade/script.js
@@ -0,0 +1,15 @@
+/*exercício 01 
+Faça uma página HTML contendo um label que exiba a mensagem “Insira sua data de nascimento”,
+um campo de texto que só permita inserir data (dica: input type date) e um botão rotulado 
+“calcular idade”. Programe uma função a ser executada quando o usuário clicar no botão, 
+que faça o cálculo da idade do usuário, e então exiba a idade em um texto na página HTML.
+*/
+
+    function calcIdade(){
+    nascimento = document.getElementById('data').value
+    diaNasc = +new Date(nascimento)
+    idade = ~~((Date.now() - diaNasc) / (31557600000))
+    document.getElementById('idade').innerHTML = 'Sua idade é: ' + idade + ' anos!'
+    return idade
+}
+
